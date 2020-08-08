c9cc7cfdc663157c683a0a5163ad52e80a325c20
shayanypn <shayanypn@gmail.com>
2020-08-08 14:45:48 +0430

    start with version 2

--- tests/integration/components/masonry-grid-test.js
+++ tests/integration/components/masonry-grid-test.js
@@ -1,25 +1,26 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
 
-moduleForComponent('masonry-grid', 'Integration | Component | masonry grid', {
-  integration: true
-});
+module('Integration | Component | masonry-grid', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    await render(hbs`<MasonryGrid />`);
 
-  this.render(hbs`{{masonry-grid}}`);
+    assert.equal(this.element.textContent.trim(), '');
 
-  assert.equal(this.$().text().trim(), '');
+    // Template block usage:
+    await render(hbs`
+      <MasonryGrid>
+        template block text
+      </MasonryGrid>
+    `);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#masonry-grid}}
-      template block text
-    {{/masonry-grid}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
 });
